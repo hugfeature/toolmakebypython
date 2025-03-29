adbe47d18b305e34927ff0bda8052ef6c028cb0a
LAPTOP-PVV52T3D\Jack <wangzhaoxian@sail-cloud.com>
2025-03-29 20:31:10 +0800

    适配国产模拟器。。。没有能用的

--- untitled/get_rewards/get_reward_phone.py
+++ untitled/get_rewards/get_reward_phone.py
@@ -25,57 +25,73 @@ def start_appium_service():
 # 启动 Appium driver
 def start_driver():
     options = UiAutomator2Options()
+    # 雷电模拟器
+    # options.platform_name = 'Android'
+    # options.platform_version = '9'
+    # options.device_name = 'emulator-5554'
+    # mumu 模拟器
     options.platform_name = 'Android'
-    options.platform_version = '15'
-    options.device_name = 'emulator-5554'
+    options.platform_version = '12'
+    options.device_name = '127.0.0.1:16384' 
     # bing
     # options.app_package = 'com.microsoft.bing'
     # options.app_activity = 'com.microsoft.sapphire.app.main.SapphireMainActivity'
+    # options.app_activity ="com.microsoft.sapphire.app.search.autosuggest.activity.AutoSuggestNativeActivity"
     # chrome
-    options.app_package = 'com.android.chrome'
-    options.app_activity = 'org.chromium.chrome.browser.ChromeTabbedActivity'
-    # options.timeouts = ''
+    # options.app_package = 'com.android.chrome'
+    # options.app_activity = 'org.chromium.chrome.browser.ChromeTabbedActivity'
+    # edge浏览器
+    options.app_package = ''
+    options.app_activity = ''
     options.no_reset = True
     driver = webdriver.webdriver.WebDriver('http://localhost:4723', options=options)
     return driver
 
 # 在 Bing 中搜索
 def search_bing(driver, keyword):
-    time.sleep(10)
-    search_box = driver.find_element(By.ID,'com.android.chrome:id/url_bar')  # 查找搜索框
+    time.sleep(20)
+    # driver.find_element(By.ID ,'com.microsoft.bing:id/iab_header_address_bar_container').click()
+    # time.sleep(3)
+    # search_box = driver.find_element(By.ID,'com.android.chrome:id/url_bar')  # 查找搜索框
+    # search_box = driver.find_element(By.ID,'com.microsoft.bing:id/input')
+    search_box = driver.find_element(By.ID,'sb_form_q')
     search_box.click()  # 点击搜索框
     search_box.send_keys(keyword)  # 输入关键词
-    search_button = driver.find_element(By.ID,'com.android.chrome:id/line_1') # 关联的第一个搜索
+    time.sleep(10)
+    # search_button = driver.find_element(By.ID,'com.android.chrome:id/line_1') # 关联的第一个搜索
+    # search_button = driver.find_element(By.XPATH,'//androidx.recyclerview.widget.RecyclerView[@resource-id="com.microsoft.bing:id/sapphire_search_list"]/android.view.ViewGroup')
+    search_button = driver.find_element(By.ID, 'sb_form')
     search_button.click()  # 提交搜索
     time.sleep(random.randint(4, 20)) # 模拟人类行为，等待随机时间
 
 # 主程序逻辑
 def main():
-    # 启动 Appium 服务
-    appium_service = start_appium_service()
-
-    # 启动 Appium Driver
-    driver = start_driver()
     search_count = 0  # 初始化计数器
     search_num = 24 # 需要搜索的次数
     time_sleep = 15 # 暂停时间
-    while search_count < search_num:
-        keyword = common_model.generate_hot_keyword()  # 随机选一个热搜词
-        search_bing(driver, keyword)
-        search_count += 1
-        logger.info(f"完成搜索 {search_count} 次,搜索的关键字：{keyword}")
-        # 每3次搜索，暂停15分钟  搜索过快会不增加积分，根据实际调整
-        if search_count % 3 == 0:
-            driver.quit()  # 退出
-            logging.info(f"已搜索{search_count}次，暂停{time_sleep}分钟...")
-            if search_count != search_num:
-                time.sleep(time_sleep * 60)  # 暂停时间，*60为分钟
-                driver = start_driver()
-                logging.info("启动appiumdriver")
+    try:
+        # 启动 Appium 服务
+        appium_service = start_appium_service()
 
-    logger.info(f"已完成{search_num}次搜索，退出程序")
-    # driver.quit() # 退出
-    appium_service.stop()  # 停止 Appium 服务
+        # 启动 Appium Driver
+        driver = start_driver()
+        while search_count < search_num:
+            keyword = common_model.generate_hot_keyword()  # 随机选一个热搜词
+            search_bing(driver, keyword)
+            search_count += 1
+            logger.info(f"完成搜索 {search_count} 次,搜索的关键字：{keyword}")
+            # 每3次搜索，暂停15分钟  搜索过快会不增加积分，根据实际调整
+            if search_count % 3 == 0:
+                driver.quit()  # 退出
+                logging.info(f"已搜索{search_count}次，暂停{time_sleep}分钟...")
+                if search_count != search_num:
+                    time.sleep(time_sleep * 60)  # 暂停时间，*60为分钟
+                    driver = start_driver()
+                    logging.info("启动appiumdriver")
+    finally:
+            logger.info(f"已完成{search_num}次搜索，退出程序")
+            # driver.quit() # 退出
+            appium_service.stop()  # 停止 Appium 服务
 
 if __name__ == "__main__":
     main()
